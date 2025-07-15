b3e68306bdf24bfda2f00edb9fc246e8a20e7ed4
GPTContribX <38401453+kiranraoboinapally@users.noreply.github.com>
2025-07-15 11:50:09 +0530

    Update OnePeopleList.jsx

--- reactJS/OnePeopleList.jsx
+++ reactJS/OnePeopleList.jsx
@@ -1,8 +1,11 @@
 export default function App() {
     const people=[
         {id:1,name:"Kiran",age:30},
-        {id:2,name:"Pavan",age:32},
-        {id:3,name:"Rakesh",age:34},
+        {id:2,name:"Akatsuki",age:32},
+        {id:3,name:"Itachi",age:34},
+        {name:"Mahi",age:40,id:5},
+        {name:"Akira",age:32},
+        {age:50,name:"Ivanka"}
     ];
   return (<>
       <h1>People List</h1>
@@ -15,3 +18,11 @@ export default function App() {
       </ul>
   </>);
 }
+
+//Output
+//Kiran-Age:30
+//Akatsuki-Age:32
+//Itachi-Age:34
+//Mahi-Age:40
+//Akira-Age:32
+//Ivanka-Age:50
