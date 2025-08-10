5a809d171cb79350bc8aca829e1771ac50ebaf69
kiranraoboinapally <kiranraoboinapally@gmail.com>
2025-08-10 20:22:36 +0530

    no imports or exports

---
+++ reactJS/reactTele/ep1/p2/App.js
@@ -0,0 +1,4 @@
+const header = React.createElement('h1',{},"Welcome to ReactDOM");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
+// ReactDOM.render(header, document.getElementById("root"));
